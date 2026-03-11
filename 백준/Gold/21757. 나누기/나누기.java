a2c81aabd3a05f04b650f92a25afc6bcdb9be24c
taejeongH <taejeongh98@gmail.com>
2026-03-11 12:22:52 +0900

    [Gold II] Title: 나누기, Time: 248 ms, Memory: 24260 KB, Score: 37 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -1,46 +1,51 @@
-// BOJ.나누기;
+//package BOJ.나누기;
 
 import java.io.*;
 import java.util.*;
 
 public class Main{
-	static int N, sum;
-	static int[] sumArr;
-	static long[][] dp;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
-		N = Integer.parseInt(br.readLine());
-		sumArr = new int[N+1];
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for (int i=1; i<=N; i++) {
-			sumArr[i] = sumArr[i-1] + (Integer.parseInt(st.nextToken()));
+		int sum = 0;
+		for (int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += arr[i];
 		}
-		sum = sumArr[N]/4;
-		dp = new long[4][N+1];
-		for (int i=0; i<4; i++) Arrays.fill(dp[i], -1);
-		System.out.println(dfs(0, 0));
-//		for (int i=0; i<4; i++) System.out.println(Arrays.toString(dp[i]));
-		
-	}
-
-	static long dfs(int depth, int start) {
-		if (dp[depth][start]!=-1) return dp[depth][start];
-		if (depth == 3) {
-			if (sumArr[N] - sumArr[start] == sum) return dp[depth][start]=1;
-			else return dp[depth][start]=0;
+		if (sum % 4 != 0) {
+			System.out.println(0);
+			return;
 		}
+		int target = sum/4;
+		int prefix = 0;
+		int count1 = 0;
+		int count2 = 0;
+		int res = 0;
 		
-		long res = 0;
-		for (int i=start+1; i<N; i++) {
-			if (sumArr[i] - sumArr[start] == sum) {
-				res += dfs(depth+1, i);
+		for (int i=0; i<N-1; i++) {
+			
+			prefix += arr[i];
+			
+			if (prefix == target*3) {
+				res += count2;
 			}
+			
+			if (prefix == target*2) {
+				count2 += count1;
+			}
+			
+			if (prefix == target) {
+				count1++;
+			}
+			
 		}
 		
-		return dp[depth][start]=res;
+		System.out.println(res);
 	}
-	
 }
+
